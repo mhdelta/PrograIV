50474479bec3ddc2a253b952ce5363108acc9f8e
miguel <miguelangelutp@gmail.com>
2018-05-28 12:27:45 -0500

    add connection trought windows

--- Proyecto final Turismo/src/turismo/controlador/PrincipalController.java
+++ Proyecto final Turismo/src/turismo/controlador/PrincipalController.java
@@ -20,6 +20,8 @@ import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static javax.swing.JOptionPane.showMessageDialog;
+import turismo.model.Paquete;
 
 /**
  *
@@ -29,6 +31,7 @@ public class PrincipalController implements ActionListener {
 
     Principal view = new Principal();
     PrincipalModel model = new PrincipalModel();
+    Paquete paquete = new Paquete();
 
     public PrincipalController(Principal view, PrincipalModel model) {
         this.view = view;
@@ -53,13 +56,15 @@ public class PrincipalController implements ActionListener {
                 try {
                     boolean res = model.ValidarFechas(view.diaI.getText(), view.mesI.getText(), view.añoI.getText(), view.diaF.getText(), view.mesF.getText(), view.añoF.getText());
                     if (res) {
+                        this.LlenarInfoPrimeraVentana(e);
+
                         SanAndres ventanaSanAndres = new SanAndres();
                         ventanaSanAndres.setVisible(true);
                         ventanaSanAndres.setLocationRelativeTo(null);
                         view.dispose();
                         System.out.print("San Andrés window open");
                     } else {
-                        System.out.println("error in fechas");
+                        showMessageDialog(null, "Ingrese una fecha correcta!");
                     }
                 } catch (ParseException ex) {
                     Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
@@ -70,13 +75,15 @@ public class PrincipalController implements ActionListener {
                 try {
                     boolean res = model.ValidarFechas(view.diaI.getText(), view.mesI.getText(), view.añoI.getText(), view.diaF.getText(), view.mesF.getText(), view.añoF.getText());
                     if (res) {
+                        this.LlenarInfoPrimeraVentana(e);
+
                         Cartagena ventanaCartagena = new Cartagena();
                         ventanaCartagena.setVisible(true);
                         ventanaCartagena.setLocationRelativeTo(null);
                         view.dispose();
                         System.out.print("Cartagena window open");
                     } else {
-                        System.out.println("error in fechas");
+                        showMessageDialog(null, "Ingrese una fecha correcta!");
                     }
                 } catch (ParseException ex) {
                     Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
@@ -87,13 +94,15 @@ public class PrincipalController implements ActionListener {
                 try {
                     boolean res = model.ValidarFechas(view.diaI.getText(), view.mesI.getText(), view.añoI.getText(), view.diaF.getText(), view.mesF.getText(), view.añoF.getText());
                     if (res) {
+                        this.LlenarInfoPrimeraVentana(e);
+
                         EjeCafetero ventanaEje = new EjeCafetero();
                         ventanaEje.setVisible(true);
                         ventanaEje.setLocationRelativeTo(null);
                         view.dispose();
                         System.out.print("Eje Cafetero window open");
                     } else {
-                        System.out.println("error in fechas");
+                        showMessageDialog(null, "Ingrese una fecha correcta!");
                     }
                 } catch (ParseException ex) {
                     Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
@@ -104,13 +113,15 @@ public class PrincipalController implements ActionListener {
                 try {
                     boolean res = model.ValidarFechas(view.diaI.getText(), view.mesI.getText(), view.añoI.getText(), view.diaF.getText(), view.mesF.getText(), view.añoF.getText());
                     if (res) {
+                        this.LlenarInfoPrimeraVentana(e);
+
                         Guatape ventanaGuatape = new Guatape();
                         ventanaGuatape.setVisible(true);
                         ventanaGuatape.setLocationRelativeTo(null);
                         view.dispose();
                         System.out.print("Guatapé window open");
                     } else {
-                        System.out.println("error in fechas");
+                        showMessageDialog(null, "Ingrese una fecha correcta!");
                     }
                 } catch (ParseException ex) {
                     Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
@@ -121,13 +132,15 @@ public class PrincipalController implements ActionListener {
                 try {
                     boolean res = model.ValidarFechas(view.diaI.getText(), view.mesI.getText(), view.añoI.getText(), view.diaF.getText(), view.mesF.getText(), view.añoF.getText());
                     if (res) {
+                        this.LlenarInfoPrimeraVentana(e);
+
                         Amazonas ventanaAmazonas = new Amazonas();
                         ventanaAmazonas.setVisible(true);
                         ventanaAmazonas.setLocationRelativeTo(null);
                         view.dispose();
                         System.out.print("amazonas window open");
                     } else {
-                        System.out.println("error in fechas");
+                        showMessageDialog(null, "Ingrese una fecha correcta!");
                     }
                 } catch (ParseException ex) {
                     Logger.getLogger(PrincipalController.class.getName()).log(Level.SEVERE, null, ex);
@@ -140,5 +153,11 @@ public class PrincipalController implements ActionListener {
             default:
                 break;
         }
+    }
+
+    public void LlenarInfoPrimeraVentana(ActionEvent e) {
+        this.paquete.ciudadDestino = e.getActionCommand();
+        this.paquete.fechaIni = view.diaI.getText() + "/" + view.mesI.getText() + "/" + view.añoI.getText();
+        this.paquete.fechaFin = view.diaF.getText() + "/" + view.mesF.getText() + "/" + view.añoF.getText();
     }
 }
---
+++ Proyecto final Turismo/src/turismo/model/Paquete.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turismo.model;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Paquete {
+    public String fechaIni, fechaFin;
+    public String ciudadDestino, hotel, excursiones;
+    public String DNI, nombreCliente;
+    public boolean extranjero, mayorista;
+    public int cuotas;
+}
